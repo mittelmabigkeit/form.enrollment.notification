bc000e7ac63e6ae05975112457245b910c7da752
mittelmabigkeit <goodnazi1488@gmail.com>
2026-01-20 14:33:54 +0300

    Create .description.php

---
+++ lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$MESS["AMIGOLAB_FORM_COMPONENT_PATH_ID"] = "local";
+$MESS["AMIGOLAB_FORM_COMPONENT_PATH_NAME"] = "Amigolab";
+$MESS["AMIGOLAB_FORM_COMPONENT"] = 'Форма "Сообщить о поступлении"';
+$MESS["AMIGOLAB_FORM_COMPONENT_DESCRIPTION"] = 'Форма "Сообщить о поступлении"';
